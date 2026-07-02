8c7bac103e0fd88bebdccc1b8d09d03bb56f5bf1
IzzyLeech <ralphhunter46@gmail.com>
2026-07-02 18:25:13 +0100

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimter from "./middleware/rateLimiter.js";
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors({
+  origin:"http://localhost:5173"
+}))
+app.use(express.json());
+app.use(rateLimter);
+
+// app.use((req, res,next) => {
+//   console.log(`Req method is  ${req.method} & Req URL is ${req.url}`);
+//   next();
+// } )
+
+app.use("/api/notes", notesRoutes);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on Port:", PORT);
+  });
+});
